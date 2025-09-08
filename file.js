ab688fb4c3679850bd90b2eae50b7d2fbf2e8e66
KashishPagarani-6 <kashishpagarani6@gmail.com>
2025-09-08 22:09:39 +0500

    side by side section of key point is finalized

--- file.js
+++ file.js
@@ -1,3 +1,11 @@
 function toggleSearch() {
   document.getElementById("search-box").classList.toggle("active");
 }
+// horizontal scroll view
+// const wrapper = document.querySelector(".cards-wrapper");
+// document.querySelector(".right-btn").addEventListener("click", () => {
+//   wrapper.scrollBy({ left: 300, behavior: "smooth" });
+// });
+// document.querySelector(".left-btn").addEventListener("click", () => {
+//   wrapper.scrollBy({ left: -300, behavior: "smooth" });
+// });
